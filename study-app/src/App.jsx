c17fb7d049c12d745adeb157b3bfb0cb86d6f7fd
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-09-26 16:29:04 +0800

    update react log to sharingan

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./img/sharingan.png";
 import "./App.css";
 
 function App() {
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>uchiha clan</p>
         <a
           className="App-link"
           href="https://reactjs.org"
