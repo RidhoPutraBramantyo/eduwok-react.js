19e3d898695d425a09c3184d03f88aca8698520f
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-10-05 16:49:09 +0800

    update

--- study-app/src/components/Navbar.jsx
+++ study-app/src/components/Navbar.jsx
@@ -5,13 +5,12 @@ import { SiAudiomack } from "react-icons/si";
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="nav-left">
+      <div className={`white-space `}></div>
+      <div className="nav">
         <div className="logo">
           <div className="rotating-image"></div>
           <h1>My Audio</h1>
         </div>
-      </div>
-      <div className="nav-right">
         <div className="user-profile">
           <img
             src="link-gambar-profil.png"
